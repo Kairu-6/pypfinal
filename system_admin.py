1faace6400b52f1f925ec9ace93665d55335f206
kai <azizikhairul666@gmail.com>
2026-02-01 23:59:03 +0800

    Implemented Add parking records

--- system_admin.py
+++ system_admin.py
@@ -19,40 +19,111 @@ def print_edit_parking_records_menu():
     print("[b] Back to Main Admin Menu")
     print("-" * 40)
 
+def load_from_file(file_name):
+    headers = []
+    data_list = []
 
-while True:
-    print_admin_menu()
+    try:
+        with open(file_name, "r") as file:
+            line = file.readline()
+            if not line:                                                # Prevents error if file is empty
+                return headers, data_list
+            headers = line.strip().split(",")
 
-    choice1 = input("Enter selection: ")
+            for line in file:
+                clean_line = line.strip()
+                if clean_line:                                          # Prevents error from empty lines
+                    entry = line.strip().split(",")
+                    data_list.append(entry)
+                
+            return headers, data_list
 
-    parking_spaces = []
-    with open("parking_spaces.txt", "r+") as ps:
-        header = ps.readline()
-        for line in ps.readlines():
-            entry = line.strip().split(",")
-            parking_spaces.append(entry)
+    except IOError:
+        print(f"[Error] Could not read file {file_name}.")
+        return headers, data_list
 
-    if choice1 == "e":
-        
-        while True:
+def save_to_file(data_list, file_name, headers):
+    try:
+        with open(file_name, "w") as file:
+            file.write(",".join(headers) + '\n')
+            for entry in data_list:
+                file.write(",".join(entry) + '\n')
+        return True
             
-            print_edit_parking_records_menu()
-            currentline = ""
-            for i in range(len(parking_spaces)):
-                data = parking_spaces[i]
-                currentline += f"{data[0]}({data[1]}) : {data[2]}" + "    "
+    except IOError:
+        print(f"[Error] Could not write to file {file_name}. Please check permissions.")
+        return False
 
-                if (i+1) % 5 == 0:
-                    print(currentline)
-                    currentline = ""
-                elif i == len(parking_spaces)-1:
-                    print(currentline)
+def get_id_number(space):
+    id = space[0]
+    id_number = int(id[1:])
+    return id_number
+
+parking_space_types = ["Regular", "Reserved", "Electric"]
+
+
+def main():
+    while True:
+        parking_headers, parking_spaces = load_from_file("parking_spaces.txt")
+
+        print_admin_menu()
+
+        admin_menu_option = input("Enter selection: ")
+
+        if admin_menu_option == "q":
+            print("Exiting Menu...")
+            break
+
+
+        elif admin_menu_option == "e":                                      # Edit Parking Records
             
-            print("")
-            choice2 = input("Enter selection: ")
+            while True:
+                
+                print_edit_parking_records_menu()
+                current_line = ""
+                for i in range(len(parking_spaces)):
+                    data = parking_spaces[i]
+                    current_line += f"{data[0]}({data[1]}) : {data[2]}" + "    "             # Parse into readable format
 
-            if choice2 == "b":
-                break
-    elif choice1 == "q":
-        print("Exiting Menu...")
-        break
+                    if (i+1) % 5 == 0:
+                        print(current_line)
+                        current_line = ""
+                    elif i == len(parking_spaces)-1:
+                        print(current_line)
+                
+                print("")
+                edit_records_option = input("Enter selection: ")
+
+
+                if edit_records_option == "b":                                      # Back To Main Menu
+                    break
+
+
+                elif edit_records_option == "a":                                    # Add New Parking Space
+                    new_type = -1
+                    while new_type not in parking_space_types:
+                        new_type = input(f"What type of parking? [{"/".join(parking_space_types)}] : ")
+
+                    existing_ids = []
+                    for space in parking_spaces:
+                        existing_ids.append(get_id_number(space))
+
+                    new_id_num = 1                                                  # Look for least non-existing id
+                    while new_id_num in existing_ids:
+                        new_id_num += 1
+
+                    if len(str(new_id_num)) > 1:                                    # Creation of new id based on id number(one digit or more)
+                        new_id = "S" + str(new_id_num)
+                    else:
+                        new_id = "S0" + str(new_id_num)
+
+                    parking_spaces.append([new_id, new_type, "Available"])
+                    parking_spaces.sort(key=get_id_number)
+
+                    if save_to_file(parking_spaces, "parking_spaces.txt", parking_headers):
+                        continue
+                    else:
+                        print("error")
+
+if __name__ == "__main__":
+    main()
